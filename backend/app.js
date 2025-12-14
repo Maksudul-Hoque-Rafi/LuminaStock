77d03b227efcf2086792a63a66fd5cf47e367cd3
maksudulhoq <maksudulhoq700@gmail.com>
2025-12-14 18:09:59 +0600

    initial backend set up

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Backend");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running");
+});
