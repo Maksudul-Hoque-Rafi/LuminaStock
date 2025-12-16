c4fec35b1dac7f9d54863fa4c1be903efd9e0015
maksudulhoq <maksudulhoq700@gmail.com>
2025-12-17 01:11:39 +0600

    cors set up

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import stockRoutes from "./routes/stockRoutes.js";
-
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello from Backend");
